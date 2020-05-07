b306f6e32939ae8580d816c67e28577505864ec8
Alan Kay <alanajkay@gmail.com>
2020-05-06 22:37:06 -0500

    Hacks until microsoft gets working 20.04 releases

--- provision/install-netcore.sh
+++ provision/install-netcore.sh
@@ -1,12 +1,16 @@
 #!/bin/zsh
 
+# Note - snap kinda works, but kinda doesn't
+sudo snap install dotnet-sdk --classic
+
+# TODO: Replace snap with this when Microsoft releases 20.04 packages. See https://github.com/dotnet/core/issues/4360
 # Install .NET Core and make it available
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo add-apt-repository universe
-sudo apt update
-sudo apt-fast install -y apt-transport-https
-sudo apt-fast install -y dotnet-sdk-3.1
+# wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+# sudo dpkg -i packages-microsoft-prod.deb
+# sudo add-apt-repository universe
+# sudo apt update
+# sudo apt-fast install -y apt-transport-https
+# sudo apt-fast install -y dotnet-sdk-3.1
 
 # Cleanup
-rm packages-microsoft-prod.deb
+# rm packages-microsoft-prod.deb
--- provision/install-powershell-core.sh
+++ provision/install-powershell-core.sh
@@ -1,19 +1,7 @@
 #!/bin/zsh
 
-# Download the Microsoft repository GPG keys
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+# Have to use preview per https://github.com/PowerShell/PowerShell-Snap#known-issues.
+sudo snap install powershell-preview --classic
 
-# Register the Microsoft repository GPG keys
-sudo dpkg -i packages-microsoft-prod.deb
-
-# Enable the "universe" repositories
-sudo add-apt-repository universe
-
-# Update the list of products
-sudo apt update
-
-# Install PowerShell
-sudo apt-fast install -y powershell
-
-# Cleanup
-rm packages-microsoft-prod.deb
+# TODO: Replace preview with this when it actually works
+# sudo snap install powershell --classic
